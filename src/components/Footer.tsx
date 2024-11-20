0b65e0fae6b7ae422064233a7c66e0469232fda2
Tru Nguyen <tru1868@gmail.com>
2024-11-19 18:14:05 -0600

    footer and responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,27 @@
+import { Link } from 'react-router-dom';
+
+import Logo from '../assets/logo-stacked.svg';
+
+import './footer.scss';
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <Link to="/" className="footer__logo">
+        <img src={Logo} />
+      </Link>
+      <div className="footer__links">
+        <a href="#" className="footer__link">
+          Terms of Use
+        </a>
+        <a href="#" className="footer__link">
+          Privacy Policy
+        </a>
+      </div>
+
+      <p>©2024 Copyright legal text.</p>
+    </footer>
+  );
 };
 
 export default Footer;
